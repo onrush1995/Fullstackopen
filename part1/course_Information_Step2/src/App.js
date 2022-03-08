67249e4b6cfe10c9017ce1723b7e699c0823a3e4
Moni <moniruzzaman.eng1995@gmail.com>
2022-03-08 15:11:14 +0200

    Exercise1.5: course information step5

--- part1/course_Information_Step2/src/App.js
+++ part1/course_Information_Step2/src/App.js
@@ -1,27 +1,31 @@
-const App = (props) => {
-    const course = 'Half Stack application development';
-    const parts = [
-        {
-            name: 'Fundamentals of React',
-            exercises: 10
-        },
-        {
-            name: 'Using props to pass data',
-            exercises: 7
-        },
-        {
-            name: 'State of a component',
-            exercises: 14
-        }
-    ];
+const App = () => {
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
+    };
 
     return (
         <div>
-            <h1>{course}</h1>
-            <p>{parts[0].name}</p>
-            <p>{parts[1].name}</p>
-            <p>{parts[2].name}</p>
-            <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+            <h1>{course.name}</h1>
+            <p>{course.parts[0].name}</p>
+            <p>{course.parts[1].name}</p>
+            <p>{course.parts[2].name}</p>
+            <p>
+                Number of exercises {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}
+            </p>
         </div>
     );
 };
