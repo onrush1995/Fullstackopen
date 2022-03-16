08b95fc11828cc0411179e063dc404477f404fbe
Moni <moniruzzaman.eng1995@gmail.com>
2022-03-16 14:08:08 +0200

    Refactoring the components

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,6 +1,29 @@
-const App = (props) => {
-    const { counter } = props;
-    return <div>{counter}</div>;
+import { useState } from 'react';
+
+const Display = ({ counter }) => <div>{counter}</div>;
+
+const Button = ({ onClick, name }) => <button onClick={onClick}>{name}</button>;
+
+const App = () => {
+    const [
+        counter,
+        setCounter
+    ] = useState(0);
+
+    const increment = () => setCounter(counter + 1);
+
+    const decrement = () => setCounter(counter - 1);
+
+    const zero = () => setCounter(0);
+
+    return (
+        <div>
+            <Display counter={counter} />
+            <Button onClick={increment} name='+' />
+            <Button onClick={decrement} name='-' />
+            <Button onClick={zero} name='0' />
+        </div>
+    );
 };
 
 export default App;
--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,13 +1,4 @@
 import ReactDOM from 'react-dom';
 import App from './App';
 
-let counter = 1;
-
-const refresh = () => {
-    ReactDOM.render(<App counter={counter} />, document.getElementById('root'));
-};
-
-setInterval(() => {
-    refresh();
-    counter += 1;
-}, 10);
+ReactDOM.render(<App />, document.getElementById('root'));
