e8f08b2c14a3b6037e534107d015e0324582b979
Moni <moniruzzaman.eng1995@gmail.com>
2022-03-17 13:55:07 +0200

    Complex state

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,27 +1,36 @@
 import { useState } from 'react';
 
-const Display = ({ counter }) => <div>{counter}</div>;
-
-const Button = ({ onClick, name }) => <button onClick={onClick}>{name}</button>;
-
 const App = () => {
     const [
-        counter,
-        setCounter
-    ] = useState(0);
+        clicks,
+        setClicks
+    ] = useState({
+        left: 0,
+        right: 0
+    });
 
-    const increment = () => setCounter(counter + 1);
+    const handleLeftClick = () => {
+        const newClicks = {
+            left: clicks.left + 1,
+            right: clicks.right
+        };
+        setClicks(newClicks);
+    };
 
-    const decrement = () => setCounter(counter - 1);
-
-    const zero = () => setCounter(0);
+    const handleRightClick = () => {
+        const newClicks = {
+            left: clicks.left,
+            right: clicks.right + 1
+        };
+        setClicks(newClicks);
+    };
 
     return (
         <div>
-            <Display counter={counter} />
-            <Button onClick={increment} name='+' />
-            <Button onClick={decrement} name='-' />
-            <Button onClick={zero} name='0' />
+            {clicks.left}
+            <button onClick={handleLeftClick}>left</button>
+            <button onClick={handleRightClick}>right</button>
+            {clicks.right}
         </div>
     );
 };
