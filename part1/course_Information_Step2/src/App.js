c6c1f65ecad29432b1b1cf49f311a0157d58de62
Moni <moniruzzaman.eng1995@gmail.com>
2022-04-06 16:35:22 +0300

    2.4: Course information step9- Completed Exercise

--- part1/course_Information_Step2/src/App.js
+++ part1/course_Information_Step2/src/App.js
@@ -1,9 +1,10 @@
 import { useState } from 'react';
 
 const App = () => {
-    const course = {
-      id: 1,
+  const courses = [
+    {
       name: 'Half Stack application development',
+      id: 1,
       parts: [
         {
           name: 'Fundamentals of React',
@@ -19,18 +20,51 @@ const App = () => {
           name: 'State of a component',
           exercises: 14,
           id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
         }
       ]
     }
+  ]
 
-  const courseNameAndExr = course.parts.map(x=><div>{x.name}-{x.exercises}</div>);
-  const exercisesCount=course.parts.map(exr=> exr.exercises)
-  const total = exercisesCount.reduce((acc,item)=> acc+item,0)
+//Half stack calculation
+const halfStack= courses.map(x=>x.parts.map(y=><div id={y}>{y.name} - {y.exercises}</div>));
+const halfStackx= courses.map(x=>x.parts.map(y=>y.exercises));
+const totalHalfStack= halfStackx[0].reduce((acc,item)=> acc + item,0);
+
+// Node Js calculation
+const nodeJs= courses.map(x=>x.parts.map(y=><div id={y}>{y.name} - {y.exercises}</div>));
+const nodeJsx= courses.map(x=>x.parts.map(y=>y.exercises));
+const totalNodeJs= nodeJsx[1].reduce((acc,item)=> acc + item,0);
+
 
 return(<div>
-    <h1>{course.name}</h1>
-    <div>{courseNameAndExr} </div>
-    <h2>Total of {total} exercises</h2>
+    <h1>Web development curriculum</h1>
+    <h2>{courses[0].name}</h2>
+    <h2>{halfStack}</h2>
+    <h2> Total of {totalHalfStack} exercises</h2>
+    <h2>{courses[1].name}</h2>
+    <h2>{nodeJs}</h2>
+    <h2> Total of {totalNodeJs} exercises</h2>
 </div>)
 }
 
