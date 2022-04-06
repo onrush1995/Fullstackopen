bfa1b009d480b8f90c35c023495fedf38eb0c325
Moni <moniruzzaman.eng1995@gmail.com>
2022-04-06 15:29:44 +0300

    2.3*: Course information step8-Completed Exercise

--- part1/course_Information_Step2/src/App.js
+++ part1/course_Information_Step2/src/App.js
@@ -23,13 +23,13 @@ const App = () => {
       ]
     }
 
-  const propsName = course.parts.map(x=><div>{x.name}-{x.exercises}</div>);
+  const courseNameAndExr = course.parts.map(x=><div>{x.name}-{x.exercises}</div>);
   const exercisesCount=course.parts.map(exr=> exr.exercises)
   const total = exercisesCount.reduce((acc,item)=> acc+item,0)
 
 return(<div>
     <h1>{course.name}</h1>
-    <div>{propsName} </div>
+    <div>{courseNameAndExr} </div>
     <h2>Total of {total} exercises</h2>
 </div>)
 }
