20d1a30dd4c7369a45049901739ec66614bc5d1c
Moni <moniruzzaman.eng1995@gmail.com>
2022-04-07 19:48:12 +0300

    Made a from of app

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,58 +1,32 @@
 import React, { useState } from 'react';
+import Note from './components/Note'
 
-const History = (props) => {
-    const newArray = props.allClicks.map((x) => x);
-    console.log(newArray);
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote,setNewNote]= useState("a new note")
 
-    const initialValue = 0;
-    const reducer = (accumulator, item) => {
-        return accumulator + item;
-    };
-    const total = props.allClicks.reduce(reducer, initialValue);
-    if (props.allClicks.length === 0) {
-        return <div>the app is used by pressing the buttons</div>;
-    }
-    return (
-        <div>
-            button press history: {props.allClicks.join(' ')}
-            <h1>{total}</h1>
-        </div>
-    );
-};
+  const addNote = (event) => {
+    event.preventDefault()
+    console.log(event.target)
+    setNewNote(event.target.value)
+  }
 
-const App = () => {
-    const [
-        left,
-        setLeft
-    ] = useState(0);
-    const [
-        right,
-        setRight
-    ] = useState(0);
-    const [
-        allClicks,
-        setAll
-    ] = useState([]);
-
-    const handleLeftClick = () => {
-        setAll(allClicks.concat(1));
-        setLeft(left + 1);
-    };
-
-    const handleRightClick = () => {
-        setAll(allClicks.concat(-1));
-        setRight(right + 1);
-    };
-
-    return (
-        <div>
-            {left}
-            <button onClick={handleLeftClick}>left</button>
-            <button onClick={handleRightClick}>right</button>
-            {right}
-            <History allClicks={allClicks} />
-        </div>
-    );
-};
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+          <input value={newNote}
+          onChange={addNote}
+          />
+          <button>Save</button>
+      </form>
+    </div>
+  )
+}
 
 export default App;
