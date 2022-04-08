e57588724adc0aeed3c4a436a7e22a5bdd94d9eb
Moni <moniruzzaman.eng1995@gmail.com>
2022-04-08 22:05:47 +0300

    Made a form in React

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,29 +1,27 @@
 import React, { useState } from 'react';
-import Note from './components/Note'
+import "./components/App.css";
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
-  const [newNote,setNewNote]= useState("a new note")
+function App(){
+const [submitting,setSubmitting]= useState(false);
+const handleSubmit = event =>{
+  event.preventDefault();
+  setSubmitting(true);
 
-  const addNote = (event) => {
-    event.preventDefault()
-    console.log(event.target)
-    setNewNote(event.target.value)
-  }
+  setTimeout(()=>{setSubmitting(false)},3000)
 
-  return (
-    <div>
-      <h1>Notes</h1>
-      <ul>
-        {notes.map(note => 
-          <Note key={note.id} note={note} />
-        )}
-      </ul>
-      <form onSubmit={addNote}>
-          <input value={newNote}
-          onChange={addNote}
-          />
-          <button>Save</button>
+}
+
+  return(
+    <div className='wrapper'>
+      <h1>How About Them Apples</h1>
+      {submitting && <div> Submitting From...</div>}
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <label>
+            <input/>
+          </label>
+        </fieldset>
+        <button type="submit">Submit</button>
       </form>
     </div>
   )
