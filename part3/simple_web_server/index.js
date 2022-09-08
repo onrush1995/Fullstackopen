e2a3c5375b241570dd7e3b0e0afc4574e52f9695
Moni <moniruzzaman.eng1995@gmail.com>
2022-09-08 09:44:34 +0300

    Simple web server has been created with the NodeJs

---
+++ part3/simple_web_server/index.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const app = http.createServer((request, response) => {
+    response.writeHead(200, { 'content-type': 'text/plain' }), response.end('Hello world');
+});
+
+const PORT = 3001;
+app.listen(PORT);
+
+console.log(`Server is running on ${PORT} `)
